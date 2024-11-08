73be615cb3821c8de142653868d473257c6cfe50
hazecodez <muhammedhashir298@gmail.com>
2024-11-08 23:33:49 +0530

    chat routes

---
+++ backend/src/routes/chatRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { sendMessage, getChatHistory } = require('../controllers/chatController');
+const router = express.Router();
+
+router.post('/send-message', sendMessage);
+router.get('/history/:userId1/:userId2', getChatHistory);
+
+module.exports = router;
