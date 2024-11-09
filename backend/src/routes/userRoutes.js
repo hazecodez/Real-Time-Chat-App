43bc5e4741a2572a1fd4578e7b20939c8e381e18
hazecodez <muhammedhashir298@gmail.com>
2024-11-09 21:44:59 +0530

    user routes updated

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { login } = require("../controllers/userController");
+const { login, register } = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/login", login);
+router.post("/register", register);
 
 module.exports = router;
