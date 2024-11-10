f9260e047a87b10c339b29705d6416ed55666695
hazecodez <muhammedhashir298@gmail.com>
2024-11-10 15:28:35 +0530

    auth context

---
+++ frontend/src/Context/AuthContext.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const login = (userData) => setUser(userData);
+  const logout = () => setUser(null);
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
