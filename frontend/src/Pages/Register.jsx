82987ae9d0077dd9fb72fb4aa153b1cb2b598cbf
hazecodez <muhammedhashir298@gmail.com>
2024-11-13 23:11:06 +0530

    register

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -9,7 +9,7 @@ const Register = () => {
     password: "",
   });
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -64,6 +64,24 @@ const Register = () => {
                 placeholder="User Name"
                 onChange={(e) =>
                   setFormData({ ...formData, username: e.target.value })
+                }
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label className="block text-sm/6 font-medium text-gray-900">
+              Email
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="text"
+                placeholder="Email"
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
                 }
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
               />
